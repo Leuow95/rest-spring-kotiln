f7f0ab07667197bf4f882eab2b0f85b9436c92d5
leonardomaia <leuow95@gmail.com>
2023-11-07 23:09:46 -0400

    Initial commit

---
+++ src/main/kotlin/br/com/leomaia/restspringkotiln/Startup.kt
@@ -0,0 +1,11 @@
+package br.com.leomaia.restspringkotiln
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestSpringKotilnApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestSpringKotilnApplication>(*args)
+}
