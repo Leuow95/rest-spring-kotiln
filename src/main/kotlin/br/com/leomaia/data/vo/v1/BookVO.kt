bda23b7529d6893d7dc287907439ef8cc6fa7d20
leonardomaia <leuow95@gmail.com>
2023-12-05 15:53:41 -0400

    fixed imports

--- src/main/kotlin/br/com/leomaia/data/vo/v1/BookVO.kt
+++ src/main/kotlin/br/com/leomaia/data/vo/v1/BookVO.kt
@@ -2,10 +2,6 @@ package br.com.leomaia.data.vo.v1
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
-import jakarta.persistence.Column
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
 import org.springframework.hateoas.RepresentationModel
 import java.util.*
 
