942066dad03c4325363f21b557c964acbab06c96
leonardomaia <leuow95@gmail.com>
2023-12-05 15:56:25 -0400

    configured cors

--- src/main/kotlin/br/com/leomaia/config/WebServerConfiguration.kt
+++ src/main/kotlin/br/com/leomaia/config/WebServerConfiguration.kt
@@ -1,7 +1,5 @@
 package br.com.leomaia.config
 
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 //@Configuration
